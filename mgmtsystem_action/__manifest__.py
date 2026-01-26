73220b115a8855cda419c487cd01bfd586b8b1c8
LP <openerp@elepeservicios.com.ar>
2026-01-26 13:25:18 -0300

    [MIG] mgmtsystem_action: Migration to 19.0

--- mgmtsystem_action/__manifest__.py
+++ mgmtsystem_action/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Management System - Action",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.0",
     "author": "Savoir-faire Linux, Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
@@ -23,5 +23,5 @@
         "views/menus.xml",
     ],
     "demo": ["demo/mgmtsystem_action.xml"],
-    "installable": False,
+    "installable": True,
 }
